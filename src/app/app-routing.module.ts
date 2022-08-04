ad1431def8595a82c8ca10ec01ee12939a43eabc
nguyenvietanh123 <anhnvth2203034@fpt.edu.vn>
2022-08-05 00:57:06 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
 import { Week1Component } from './component/week1/week1.component';
 import { Week2Component } from './component/week2/week2.component';
 
@@ -12,6 +14,14 @@ const routes: Routes = [
     path: 'week2',
     component: Week2Component,
   },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './component/sidebar/sidebar.component';
@@ -8,6 +7,9 @@ import { Week1Component } from './component/week1/week1.component';
 import { Week2Component } from './component/week2/week2.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,10 @@ import { HeaderComponent } from './component/header/header.component';
     Week1Component,
     Week2Component,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
